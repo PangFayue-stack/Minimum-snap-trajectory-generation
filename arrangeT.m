6fbbe4a2ab91da8437a4a1bace7abb5f48670224
Pang Fayue <2270269366@qq.com>
2024-10-21 16:03:17 +0800

    finish arrangeT

--- arrangeT.m
+++ arrangeT.m
@@ -1,1 +1,13 @@
-function time_interval(path, T)
+function time_interval = arrangeT(path, T)
+    n = size(path,1);
+    sum = zeros(1,n);
+    for i=2:n
+        sum(i) = distance(path(i-1,:)',path(i,:)') + sum(i-1);
+    end
+    time_interval = sum/sum(n)*T;
+end
+
+function dist = distance(point1, point2) %column vector
+    d = point1 - point2;
+    dist = sqrt(d' * d);
+end
